63ff7296f64ea3def4746b9b06d9c7a793ac5d5a
edgaras5695 <Edgaras.Valansevicius@stud.vtmc.lt>
2024-04-24 10:15:34 +0300

    PD20

---
+++ PD20/PD20.1/src/Card.jsx
@@ -0,0 +1,12 @@
+function Card() {
+  return ( 
+    <div className="kortele">
+      <img src="" alt="" />
+      <h1>aprasymas</h1>
+      <p>tekstas</p>
+      <span>kaina</span>
+    </div>
+   );
+}
+
+export default Card;

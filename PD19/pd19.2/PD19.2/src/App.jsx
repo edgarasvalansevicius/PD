70a6d3dd1cab0bf939f459b25d6a471b04a3df3d
edgaras5695 <Edgaras.Valansevicius@stud.vtmc.lt>
2024-04-24 10:15:04 +0300

    pd19

---
+++ PD19/pd19.2/PD19.2/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  return(
+
+
+  
+    const [text, setText] = useState ('Pradinis tekstas');
+    
+    function changeText (){}
+    setText ('Naujas tekstas');
+  )
+};
+
+export default App
